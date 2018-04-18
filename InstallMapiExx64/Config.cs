acbbd25282149c355ffdebe8e788bc08c175753f
Rod Stewart <rodstewart@gmail.com>
2018-04-17 21:41:31 -0500

    Refactors Config.cs

--- InstallMapiExx64/Config.cs
+++ InstallMapiExx64/Config.cs
@@ -5,24 +5,34 @@
         public static string DestinationDirectory = @"C:\Program Files\Blue Prism Limited\Blue Prism Automate\";
         public static string SourceDirectory = @"C:\Program Files (x86)\Blue Prism Limited\Blue Prism Automate\";
 
-        public static string Mapi32DllSource = SourceDirectory + @"MAPI32.dll";
-        public static string Mapi32DllTarget = DestinationDirectory + @"MAPI32.dll";
+        public static string VboTargetDirectory = DestinationDirectory + @"VBO\";
+        public static string VboSourceDirectory = SourceDirectory + @"VBO\";
 
-        public static string MapiexDllSource = SourceDirectory + @"MAPIEx.dll";
-        public static string MapiexDllTarget = DestinationDirectory + @"MAPIEx.dll";
+        private const string Mapi32DllFilename = "MAPI32.dll";
+        private const string MapiexDllFilename = "MAPIEx.dll";
+        private const string MapiexAutomationDllFilename = "BluePrismMAPIExAutomation.dll";
+        private const string MapiexAutomationTlbFilename = "BluePrismMAPIExAutomation.tlb";
+        private const string MapiExHelpFilename = "BPMapiEx.chm";
+        private const string MapiexVboFilename = "BPA Object - Blue Prism MAPIEx.xml";
 
-        public static string MapiexAutomationDllSource = SourceDirectory + @"BluePrismMAPIExAutomation.dll";
-        public static string MapiexAutomationDllTarget = DestinationDirectory + @"BluePrismMAPIExAutomation.dll";
 
-        public static string MapiexAutomationTlbSource = SourceDirectory + @"BluePrismMAPIExAutomation.tlb";
-        public static string MapiexAutomationTlbTarget = DestinationDirectory + @"BluePrismMAPIExAutomation.tlb";
+        public static string Mapi32DllSource = SourceDirectory + Mapi32DllFilename;
+        public static string Mapi32DllTarget = DestinationDirectory + Mapi32DllFilename;
 
-        public static string MapiExHelpSource = SourceDirectory + @"BPMapiEx.chm";
-        public static string MapiExHelpTarget = DestinationDirectory + @"BPMapiEx.chm";
+        public static string MapiexDllSource = SourceDirectory + MapiexDllFilename;
+        public static string MapiexDllTarget = DestinationDirectory + MapiexDllFilename;
 
-        public static string MapiexVboSource = SourceDirectory + @"VBO\BPA Object - Blue Prism MAPIEx.xml";
-        public static string MapiexVboTarget = DestinationDirectory + @"VBO\BPA Object - Blue Prism MAPIEx.xml";
+        public static string MapiexAutomationDllSource = SourceDirectory + MapiexAutomationDllFilename;
+        public static string MapiexAutomationDllTarget = DestinationDirectory + MapiexAutomationDllFilename;
 
-        public static string VboTargetDirectory = DestinationDirectory + @"VBO";
+        public static string MapiexAutomationTlbSource = SourceDirectory + MapiexAutomationTlbFilename;
+        public static string MapiexAutomationTlbTarget = DestinationDirectory + MapiexAutomationTlbFilename;
+
+        public static string MapiExHelpSource = SourceDirectory + MapiExHelpFilename;
+        public static string MapiExHelpTarget = DestinationDirectory + MapiExHelpFilename;
+
+        public static string MapiexVboSource = VboSourceDirectory + MapiexVboFilename;
+        public static string MapiexVboTarget = VboTargetDirectory + MapiexVboFilename;
+
     }
 }
